b4cfbc55a32b158ee159fa0d04ba37efce704f17
Ryandra Rama Syahputra <20390100005@dinamika.ac.id>
2022-06-04 21:13:33 +0700

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
-const generateImage = require("./generateImage")
+
 
 const client = new Discord.Client({
     intents: [
@@ -11,24 +11,22 @@ const client = new Discord.Client({
     ]
 })
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`)
-})
+let bot = {
+    client, 
+    prefix: "!",
+    owners: "982272869163749446"
+}
 
-client.on("messageCreate", (message) => {
-    if (message.content == "hi"){
-        message.reply("Hello World!")
-    }
-})
+client.commands = new Discord.Collection()
+client.events = new Discord.Collection()
 
-const welcomeChannelId = "979214166701277194"
+client.loadEvents = (bot, reload) => require ("./handlers/events")(bot, reload)
+client.loadCommands = (bot, reload) => require ("./handlers/commands")(bot, reload)
 
-client.on("guildMemberAdd", async (member) => {
-    const img = await generateImage(member)
-    member.guild.channels.cache.get(welcomeChannelId).send({
-        content: `<@${member.id}> Welcome to the server!`,
-        files: [img]
-    })
-})
+client.loadEvents(bot, false)
+client.loadCommands(bot, false)
+
+module.exports = bot 
+
 
 client.login(process.env.TOKEN)
